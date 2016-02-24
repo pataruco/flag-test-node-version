0b9709d78e6dc1db30faf3c63029c565e076495a
pataruco <pataruco@gmail.com>
2016-02-24 17:39:41 +0000

    Add database connection when the server starts

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
-var express = require('express');
-var app = express();
-var routes = require("./config/routes");
-// var server = require('http').createServer(app);
-var morgan = require('morgan');
-var request = require('request');
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
-// var db = require('models/models');
+var express = require('express'),
+    app = express(),
+    routes = require("./config/routes"),
+    morgan = require('morgan'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    mongoose = require ("mongoose"),
+    uristring = process.env.MONGOLAB_URI ||
+                process.env.MONGOHQ_URL ||
+                'mongodb://localhost:27017/flags',
+    port = process.env.PORT || 3000;
 
 //middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
+// Server console
 app.use(function(req, res, next) {
   console.log('%s request to %s from %s', req.method, req.path, req.ip);
   next();
 });
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+//Database connection
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
 
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
+//Router
 app.use('/', routes);
-// app.use('/principles', principlesRouter)
 
 //Server
 app.listen(port);

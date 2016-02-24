be54c78bcbe3d315a85d261e0b0c328b8bc3cf20
pataruco <pataruco@gmail.com>
2016-02-24 11:48:27 +0000

    Create Principle model

--- models.js
+++ models.js
@@ -38,7 +38,27 @@ var flagSchema = new mongoose.Schema({
   }
 });
 
+var principleSchema = new mongoose.Schema({
+  order: {
+    type: Number,
+    default: 0
+  },
+  title: {
+    type: String,
+    default: ""
+  },
+  subTitle: {
+    type: String,
+    // default: ""
+  },
+  explanation: {
+    type: String,
+    default: true
+  }
+});
+
+
 var Flag = mongoose.model("Flag", flagSchema);
-// var Comment = mongoose.model("Comment", CommentSchema);
+var Principle = mongoose.model("Principle", principleSchema);
 module.exports.Flag = Flag;
-// module.exports.Comment = Comment;
+module.exports.Principle = Principle;

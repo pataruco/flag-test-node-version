9d0430302da870d81ff31a10bc4c23f184f2baeb
pataruco <pataruco@gmail.com>
2016-02-27 13:23:53 +0000

    Add Mongoose connection to each model

--- models/principle.js
+++ models/principle.js
@@ -1,5 +1,17 @@
 var mongoose = require ("mongoose");
 
+uristring = process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            'mongodb://localhost:27017/flags';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
 var principleSchema = new mongoose.Schema({
   order: {
     type: Number,

8705873fd24c7952956d54fe5adafe37a30a3f7c
pataruco <pataruco@gmail.com>
2016-03-04 11:58:04 +0000

    Refactor all code

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,6 @@
 var express = require('express'),
     router  = express.Router(),
     bodyParser = require('body-parser'), //parses information from POST
-    methodOverride = require('method-override'), //used to manipulate POST
-    principlesController = require('../controllers/principles'),
     Principle = require('../models/principle'),
     db = require('../models/principle'),
     Flag = require('../models/flag');
@@ -11,31 +9,20 @@ var express = require('express'),
 // Root
 router.get('/', function(request, response) {
   console.log("Root");
-  var data = {};
   db = require('../models/principle');
   db.Principle.find(function(error, principles) {
     if(error) response.json({message: 'Could not find any principle'});
-    data.principles =  principles;
+    response.render('root', {principles});
   });
-    db = require('../models/flag');
-    db.Flag.find(function(error, flags) {
-      if(error) response.json({message: 'Could not find any flag'});
-      data.flags =  flags;
-      response.render('root', {data: data});
-    });
 });
 
+// Quiz
 router.get('/quiz', function(request, response){
   console.log("Quiz");
-  var data = {};
-  db = require('../models/flag');
-  db.Flag.find(function(error, flags) {
-    if(error) response.json({message: 'Could not find any flag'});
-    data.flags =  flags;
-    response.render('quiz', {data: data});
-  });
+  response.render('quiz');
 });
 
+// Flags AJAX request
 router.get('/flags', function(request, response){
   console.log("Quiz");
   db = require('../models/flag');
@@ -45,98 +32,17 @@ router.get('/flags', function(request, response){
   });
 });
 
+// Watch
 router.get('/watch', function(request, response){
   console.log('watch');
   response.render('watch');
 });
 
-
-//Admin page
-router.get('/admin', function(request, response){
-  console.log('Admin');
-  response.render('admin');
-});
-
+// About
 router.get('/about', function(request, response){
-  console.log('about');
+  console.log('Admin');
   response.render('about');
 });
-
-
-
-// Principles roots
-// router.route('/principles')
-//
-//   //GET all principles
-//   .get(principlesController.getAll)
-//
-//   //POST a new principle
-//   .post(principlesController.createPrinciple);
-//
-// router.route('/principles/:id')
-//
-//   // GET return specific principle
-//   .get(principlesController.getPrinciple)
-//
-//   // PATCH update existing principle
-//   .patch(principlesController.updatePrinciple)
-//
-//   // DELETE remove specific principle from DB
-//   .delete(principlesController.removePrinciple);
-
-// router.route('/principles/new')
-//   .get(principlesController.newPrinciple);
-// router.get('/', function(req, res) {
-//   console.log("Root");
-//   res.render("index");
-// });
-//
-// // Principles CRUD
-// // Index
-// router.get('/principles', function(req, res) {
-//   console.log("index");
-//   db.Principle.find({}, function(err, principles){
-//     res.send(principles);
-//     // res.render("principles/index")
-//   })
-// });
-//
-// // New
-// router.get('/principles/new', function(req, res) {
-//   console.log("new");
-//   res.render("principles/new");
-// });
-//
-// // Show
-// router.get('/principles/:id', function(req, res) {
-//   console.log("show");
-//   res.send("SHOW");
-// });
-//
-// // Create
-// router.post('/principles', function(req, res) {
-//   console.log("create");
-//   console.log(req.body)
-//   // res.send("CREATE");
-// });
-//
-// // Edit
-// router.get('/principles/:id/edit', function(req, res) {
-//   console.log("edit");
-//   res.send("EDIT");
-// });
-//
-// // Update
-// router.put('/principles/:id', function(req, res) {
-//   console.log("update");
-//   res.send("UPDATE");
-// });
-//
-// // Delete
-// router.delete('/posts/:id', function(req, res) {
-//   console.log("delete");
-//   res.send("DELETE");
-// });
 
 // Making available for all
 module.exports = router;

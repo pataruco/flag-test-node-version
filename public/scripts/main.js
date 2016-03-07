8f6479dea3fd32d3b70a1f0b0c52832a1de45572
pataruco <pataruco@gmail.com>
2016-03-07 11:00:26 +0000

    Animate Quiz page

---
+++ public/scripts/main.js
@@ -0,0 +1,10 @@
+//Extend Animation CSS to jQuery
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
